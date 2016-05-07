df8eee3504bcccfabc1c1a54d5631d60694d0070
Befezdow <befezd@gmail.com>
2016-05-07 23:33:50 +0400

    Added files via upload

--- func.h
+++ func.h
@@ -1,4 +1,5 @@
 #include <cmath>
+#include <string>
 #define sp1 80
 #define sp2 70
 #define sp3 50
@@ -34,7 +35,7 @@ class car // класс-машина
 {
     time_t time; // время, на которое машина занята
     int type;   // тип машины (1-для взрослого человека, 2-для ребенка, 3-для пожилых людей)
-    int num;    // номер машины (ее ведь надо как-то идентифицировать)
+    string num;    // номер машины (ее ведь надо как-то идентифицировать)
     float sp; // скорость машины
     float fl; // топливо на км
   public:
@@ -44,7 +45,6 @@ class car // класс-машина
     void move(const call &a, const control &ctrl); /*эта функция двигает машину в указанную точку,  добавляет время на дорогу и оказываемую услугу (1-оказать первую помощь, 2-довести до больницы). 
     Если нужно оказать первую помощь, то добавляется время на дорогу и на оказание первой помощи (в среднем 20 минут), loc_st меняются на координаты последнего больного. 
     Если больного нужно довести до больницы, то время добавляется на дорогу до больного и дорогу от больного до больницы, соответственно, loc_st машины меняются на местоположение больницы. */
-    float def_dis(const coord &a); //определение расстояния от машины до точки, использовать только если машина свободна!
     time_t get_time() { return time; }
     int get_type() { return type; }
     int get_num() { return num; }
@@ -64,17 +64,6 @@ class control
     control();
 }
 
-float car::def_dis(const coord &a) //определение расстояния для машины, аргумент - конечная точка
-{
-  coord cur=get_loc_cur();
-  float x=cur.get_x();
-  float y=cur.get_y();
-  float k=a.get_x();
-  float m=a.get_y();
-  float p=pow(pow(k-x,2)+pow(m-y,2),0.5);
-  return p;
-}
-
 float coord::def_dis(const coord &a) //определение расстояния для стационарной точки
 {
   float k=a.get_x();
@@ -83,9 +72,16 @@ float coord::def_dis(const coord &a) //определение расстояни
   return p;
 }
 
-coord car::get_loc_cur()
+coord car::get_loc_cur() //функция возвращает текущее положение машины. Её вызывать только если машина свободна!
+//если машина занята, и вы вызовете эту функцию, то она вернет обнуленный объект типа коорд
 {
   coord back;
+  if (time()-time<0) // проверка на корректное использование. здесь можно вызвать экстренную ситуацию
+  {
+    cout<<"Некорректный вызов функции get_loc_cur()"<<endl;
+    back.set_coord(0,0);
+    return back;
+  }
   float x=loc_st.get_x();
   float y=loc_st.get_y();
   float k=loc_prim.get_x();
@@ -109,17 +105,19 @@ coord car::get_loc_cur()
   return back;
 }
   
-void car::move(const call &a, const control &ctrl)
+void car::move(const call &a, const control &ctrl) //функция двигает машину в точку заказа и добавляет время
 {
-  if (a.get_cserv()==1) 
+  if (a.get_cserv()==1) // если оказать первую помощь
   {
-    float dis=def_dis(a.get_cloc());
+    coord tab=get_loc_cur();
+    float dis=tab.def_dis(a.get_cloc());
     loc_st.set_coord(a.get_cloc());
     time=time(NULL)+dis/sp*3600+1200;
   }
-  if (a.get_cserv()==2)
+  if (a.get_cserv()==2) // если вести до больницы
   {
-    float dis1=def_dis(a.get_cloc());
+    coord tab=get_loc_cur();
+    float dis1=tab.def_dis(a.get_cloc());
     coord fol=a.get_cloc();
     float dis2=fol.def_dis((&ctrl)->bbeg); 
     loc_st.set_coord((&ctrl)->bbeg);
